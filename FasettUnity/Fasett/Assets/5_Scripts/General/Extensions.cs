303d45973f1458dbf95c4e0ad26787153f87ad69
Björn Albihn <bjorn@albihn.net>
2018-03-24 23:09:36 +0100

    move vitoria

---
+++ FasettUnity/Fasett/Assets/5_Scripts/General/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions {
+
+	//Even though they are used like normal methods, extension
+	//methods must be declared static. Notice that the first
+	//parameter has the 'this' keyword followed by a Transform
+	//variable. This variable denotes which class the extension
+	//method becomes a part of.
+	public static GradientAlphaKey Lerp(this GradientAlphaKey low, GradientAlphaKey high, float value)
+	{
+		float time = Mathf.Lerp(low.time, high.time, value);
+		float alpha = Mathf.Lerp(low.alpha, high.alpha, value);
+		return new GradientAlphaKey(alpha, time);
+	}
+}
---
+++ FasettUnity/Fasett/Assets/5_Scripts/UI/TestSlider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestSlider : MonoBehaviour {
+	[SerializeField] private Slider _slider;
+	[SerializeField] private FloatInput _floatInput;
+
+	private void Start () {
+		_slider.onValueChanged.AddListener((value) => _floatInput.SetNewValue(value));	
+	}
+}
