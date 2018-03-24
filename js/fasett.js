4992d858af52c9d4d165dd671226d7fcc704453e
Jonas Johansson <jns.johansson@gmail.com>
2018-03-24 20:34:51 +0100

    added third party folder and socket.io unity

---
+++ js/fasett.js
@@ -0,0 +1,37 @@
+// https://github.com/russellmcc/node-osc-min
+// https://github.com/alexanderwallin/harpaio-music-engine/blob/master/src/resolume-osc.js
+var osc = require('node-osc');
+var five = require("johnny-five");
+
+// http://johnny-five.io/api/board/
+const board = new five.Board({
+	port: '/dev/cu.usbmodem1411',
+	repl: false,
+	debug: false
+});
+
+board.on('ready', function() {
+
+	const client = new osc.Client('127.0.0.1', 6969);
+
+	for (const input of [2]){
+
+		// http://johnny-five.io/api/sensor/
+		const sensor = new five.Sensor.Digital(input);
+
+		sensor.on('change', (value) => {
+
+			// scale 0-1023 to 0-127
+			// let value = sensor.scaleTo(0,127);
+
+			const msg = '/d'+input;
+
+			console.log(msg, value);
+
+			client.send(msg, value, function (){
+				// client.kill();
+			});
+		});
+	}
+
+});
