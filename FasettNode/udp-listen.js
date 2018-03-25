a710ed41ccb1905160ebf1ee2134b252fddeac77
Jonas Johansson <jns.johansson@gmail.com>
2018-03-25 17:07:41 +0200

    renamed files…

--- FasettNode/udp-listen.js
+++ FasettNode/udp-listen.js
@@ -1,15 +1,15 @@
 var PORT = 7002;
 var dgram = require('dgram');
-var client = dgram.createSocket('udp4');
+var socket = dgram.createSocket('udp4');
 
-client.on('listening', function () {
-    var address = client.address();
+socket.on('listening', function () {
+    var address = socket.address();
     console.log('UDP Client listening on ' + address.address + ":" + address.port);
-    client.setBroadcast(true);
+    socket.setBroadcast(true);
 });
 
-client.on('message', function (message, rinfo) {
+socket.on('message', function (message, rinfo) {
     console.log('Message from: ' + rinfo.address + ':' + rinfo.port +' - ' + message);
 });
 
-client.bind(PORT);
+socket.bind(PORT);
