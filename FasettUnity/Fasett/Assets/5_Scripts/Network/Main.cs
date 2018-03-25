0c8f6f9986e03a486c1b914a602772ac6dc42708
BjornPC <bjorn@albihn.net>
2018-03-25 10:52:11 -0700

    Adding new broadcastreciever in hololens project

---
+++ FasettUnity/Fasett/Assets/5_Scripts/Network/Main.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class Main : MonoBehaviour {
+
+    public InputManager inputManager;
+    public BroadcastReceiver receiver = new BroadcastReceiver ();
+	// Use this for initialization
+	void Start () {
+		receiver.Receive (7003);
+		receiver.MessageReceived += Log;
+	}
+
+    private void OnDisable()
+    {
+        receiver.Dispose();
+    }
+
+    // Update is called once per frame
+    void Log (string message, IPEndPoint remoteEndpoint) {
+		Debug.Log (message);
+      //  inputManager.TestSetFloat(Mathf.InverseLerp(0,127,);  
+    }
+}
