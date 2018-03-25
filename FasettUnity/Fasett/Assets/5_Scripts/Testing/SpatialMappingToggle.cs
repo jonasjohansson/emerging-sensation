926e1db3336656616ac7191a0aed8cf6d22673c4
BjornPC <bjorn@albihn.net>
2018-03-25 09:22:03 -0700

    Fasett is now running on Hololens

---
+++ FasettUnity/Fasett/Assets/5_Scripts/Testing/SpatialMappingToggle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+using UnityEngine.XR.WSA.Input;
+
+public class SpatialMappingToggle : MonoBehaviour {
+
+    [SerializeField] private SpatialMappingRenderer spatialMappingRenderer;
+    private GestureRecognizer recognizer;
+
+    private void Start()
+    {
+        // Set up our GestureRecognizer and listen for Tapped
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += (source, tapCount, ray) =>
+        {
+            ToggleRendering();
+        };
+        recognizer.StartCapturingGestures();
+    }
+
+    private void ToggleRendering(){
+        if (spatialMappingRenderer.renderState != SpatialMappingRenderer.RenderState.Occlusion)
+            spatialMappingRenderer.renderState = SpatialMappingRenderer.RenderState.Occlusion;
+        else
+            spatialMappingRenderer.renderState = SpatialMappingRenderer.RenderState.Visualization;
+    }
+}
