edd7d86de58f80ea0c7ab5c77d075763ef99a887
Jonas Johansson <jns.johansson@gmail.com>
2018-10-03 12:09:27 +0200

    Coords functioning (error was 8bit int…)

--- FasettArduino/Body/Body.ino
+++ FasettArduino/Body/Body.ino
@@ -5,51 +5,57 @@
  */
  
 #include<FastLED.h>
+//#include <Adafruit_NeoPixel.h>
 
 #define NUM_LEDS 289
 #define NUM_PARTS 26
 #define LED_PIN 6
-#define BRIGHTNESS 128
+#define BRIGHTNESS 32
 
 CRGB leds[NUM_LEDS];
+//Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 int lights[NUM_PARTS][3] = {
-  { 0, 9, 1 },
-  { 9, 6, 1 },
-  { 15, 16, 1 }, // large ball 1 outside
-  { 31, 31, 2 }, // large ball 1 inside
-  { 62, 8, 1 },
-  { 70, 9, 1 },
-  { 79, 20, 2 }, // small ball 2 inside
-  { 99, 20, 2 }, // small ball 3 inside
-  { 119, 9, 1 },
-  { 128, 7, 1 },
-  { 135, 6, 1 },
-  { 141, 8, 1 },
-  { 149, 7, 1 },
-  { 156, 9, 1 },
-  { 165, 5, 1 },
-  { 170, 5, 1 },
-  { 175, 8, 1 },
-  { 183, 6, 1 },
-  { 189, 5, 1 },
-  { 194, 12, 1 },
-  { 206, 23, 2 }, // large ball 4 outside
-  { 229, 8, 1 }, // large ball 4 inside
-  { 237, 27, 1 },
-  { 264, 14, 1 }, // large ball 5 inside
-  { 278, 6, 1 }, // large ball 5 outside
-  { 284, 5, 1 }
+  { 0, 9, 1 },    // 0
+  { 9, 6, 1 },    // 1
+  { 15, 16, 1 },  // 2 large ball 1 outside
+  { 31, 31, 2 },  // 3 large ball 1 inside
+  { 62, 8, 1 },   // 4
+  { 70, 9, 1 },   // 5
+  { 79, 20, 2 },  // 6 small ball 2 inside
+  { 99, 20, 2 },  // 7 small ball 3 inside
+  { 119, 9, 1 },  // 8
+  { 128, 7, 1 },  // 9
+  { 135, 6, 1 },  // 10
+  { 141, 8, 1 },  // 11
+  { 149, 7, 1 },  // 12
+  { 156, 9, 1 },  // 13
+  { 165, 5, 1 },  // 14
+  { 170, 5, 1 },  // 15
+  { 175, 8, 1 },  // 16
+  { 183, 6, 1 },  // 17
+  { 189, 5, 1 },  // 18
+  { 194, 12, 1 }, // 19 large ball 4 outside
+  { 206, 23, 2 }, // 20 large ball 4 inside
+  { 229, 8, 1 },  // 21
+  { 237, 27, 2 }, // 22 large ball 5 inside
+  { 264, 14, 1 }, // 23 large ball 5 outside
+  { 278, 6, 1 },  // 24 
+  { 284, 5, 1 },  // 25
 };
-
 void setup() {
   Serial.begin(115200);
   FastLED.addLeds<NEOPIXEL,LED_PIN>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
-  resetLights();
-  setColorAll(0,255,0);
+  //setColorAll(0,0,0);
   setColor(2,255,0,0);
   setColor(3,0,0,255);
+  setColor(6,255,0,0);
+  setColor(7,255,0,0);
+  setColor(19,255,0,0);
+  setColor(20,0,0,255);
+  setColor(22,0,255,0);
+  setColor(23,255,0,0);
 }
 
 void loop(){
