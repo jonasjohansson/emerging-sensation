0065c6494cf96e94efc91168bf617591ae92302b
Jonas Johansson <jns.johansson@gmail.com>
2018-11-06 23:04:35 +0100

    Update FasettArduino/FasettSky3/FasettSky3.ino

--- FasettArduino/FasettSky3/FasettSky3.ino
+++ FasettArduino/FasettSky3/FasettSky3.ino
@@ -15,28 +15,31 @@
 #define NUM_SENSORS 4
 #define OUT_MIN 0
 #define OUT_MAX 100
+//#define DEBUG
 
 #define BRIGHTNESS 32
 
 CRGB leds[NUM_STRIPS*NUM_LEDS];
 
 static byte wingA[NUM_WINGS][2] = { {0,0}, {0,1}, {6,0}, {6,1} }; // x, y
-static byte wingB[NUM_WINGS][2] = { {1,1}, {1,2}, {0,1}, {0,2} };
-static byte wingC[NUM_WINGS][2] = { {2,0}, {2,1}, {1,0}, {1,1} };
-static byte wingD[NUM_WINGS][2] = { {3,1}, {3,2}, {2,1}, {2,2} };
-static byte wingE[NUM_WINGS][2] = { {4,0}, {4,1}, {3,0}, {3,1} };
-static byte wingF[NUM_WINGS][2] = { {5,1}, {5,2}, {4,1}, {4,2} };
-static byte wingG[NUM_WINGS][2] = { {6,0}, {6,1}, {5,0}, {5,1} };
-static byte wingH[NUM_WINGS][2] = { {7,1}, {7,2}, {6,1}, {6,2} };
+//static byte wingB[NUM_WINGS][2] = { {1,1}, {1,2}, {0,1}, {0,2} };
+//static byte wingC[NUM_WINGS][2] = { {2,0}, {2,1}, {1,0}, {1,1} };
+//static byte wingD[NUM_WINGS][2] = { {3,1}, {3,2}, {2,1}, {2,2} };
+//static byte wingE[NUM_WINGS][2] = { {4,0}, {4,1}, {3,0}, {3,1} };
+//static byte wingF[NUM_WINGS][2] = { {5,1}, {5,2}, {4,1}, {4,2} };
+//static byte wingG[NUM_WINGS][2] = { {6,0}, {6,1}, {5,0}, {5,1} };
+//static byte wingH[NUM_WINGS][2] = { {7,1}, {7,2}, {6,1}, {6,2} };
 
 int sensors[NUM_SENSORS][4] = {
-  {0,0,100,200}, // pin, value, min, max
-  {1,0,100,200},
-  {2,0,100,200},
-  {3,0,100,200} };
+  {17,0,100,200}, // pin, value, min, max
+  {18,0,100,200},
+  {19,0,100,200},
+  {22,0,100,200} };
 
 void setup() {
-  Serial.begin(115200);
+  #ifdef DEBUG
+    Serial.begin(115200);
+  #endif
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.addLeds<WS2812,2>(leds, NUM_LEDS);
   FastLED.addLeds<WS2812,14>(leds, NUM_LEDS);
@@ -50,7 +53,7 @@ void setup() {
 
 void loop(){
   readSensors();
-  colorise(wingA);
+  colorise(wingA,255,255,255);
 }
 
 void readSensors(){
@@ -62,17 +65,23 @@ void readSensors(){
     val = constrain(val,in_min,in_max);
     val = map(val,in_min,in_max,OUT_MIN,OUT_MAX);
     sensors[i][1] = val;
+    #ifdef DEBUG
+      Serial.print("Sensor (");
+      Serial.print(i);
+      Serial.print("): ");
+      Serial.println(val);
+    #endif
   }
 }
 
-void colorise(byte wing[][2]){
+void colorise(byte wing[][2], byte r, byte g, byte b){
   for (byte i = 0; i < NUM_WINGS; i++){
     byte x = wing[i][0];
     byte y = wing[i][1];
     uint8_t start = NUM_LEDS_PER_SIDE * y;
     uint8_t end = NUM_LEDS_PER_SIDE * (y + 1);
     for (uint8_t j = start; j < end; j++){
-      FastLED[x][j] = CRGB(255,255,255);
+      FastLED[x][j] = CRGB(r,g,b);
     }
   }
   FastLED.show();
