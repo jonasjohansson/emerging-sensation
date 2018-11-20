770b9766d5a16d8e1d75ec5aaeae8fb90c0f50a4
JonatanCrafoord <jonatan@crafoord.net>
2018-11-20 15:23:40 +0100

    * Swapped test effects for ones actually showing the value being received.

---
+++ FasettUnity/Fasett/Assets/5_Scripts/Effects/DebugEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fasett {
+    public class DebugEffect : Effect {
+        [SerializeField] private TextMesh _valueText;
+
+        public override void UpdateEffect(float value) {
+            base.UpdateEffect(value);
+            _valueText.text = value.ToString();
+        }
+    }
+}
