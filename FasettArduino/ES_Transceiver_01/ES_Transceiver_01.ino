48c6a372e4cfc740dda1ec91950fa898382a36a2
Jonas Johansson <jns.johansson@gmail.com>
2018-11-21 12:36:40 +0100

    Transceiver update

--- FasettArduino/ES_Transceiver_01/ES_Transceiver_01.ino
+++ FasettArduino/ES_Transceiver_01/ES_Transceiver_01.ino
@@ -1,53 +1,38 @@
-#define NUM_PINS 8
-
-byte pins[NUM_PINS] = {2,14,7,8,6,20,21,5};
+#define THRESH 3
 
 int al, bl, cl;
 
 void setup(){
-  
-  for (byte i = 0; i < NUM_PINS; i++)
-    pinMode(i, INPUT);
-  
   Serial.begin(115200);
 }
 
 void loop(){
 
-  // read
-  int a = analogRead(2);
-  int b = analogRead(14);
-  int c = analogRead(7);
-  
-  // filter
-  a = f(a,50,200);
-  b = f(b,50,200);
-  c = f(c,50,200);
+  int a = r(23);
 
-  // send
-  if (a != al){
+  // diff
+  int adiff = abs(a - al);
+  if (adiff > THRESH){
     al = a;
+    // filter
+    a = f(a,30,120); 
+    // send
     s(0,a);
   }
+}
 
-  if (b != bl){
-    bl = b;
-    s(1,b);
-  }
-
-  if (c != cl){
-    cl = c;
-    s(2,c);
-  }
+int r(int pin){
+  int val = analogRead(pin);
+  return smooth(val);
 }
 
 int f(int val, int min, int max){
   val = constrain(val,min,max);
-  val = map(val,min,max,0,255);
+  val = map(val,min,max,255,0);
   return val;
 }
 
 void s(byte index, byte val){
-  Serial.print(index+' ');
+  Serial.print(String(index)+' ');
   Serial.println(val);
 }
