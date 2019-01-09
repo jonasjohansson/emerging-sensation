4d82ec7fa59e8c0d43afc23e3aa5f389da1d4e4e
Jonas Johansson <jns.johansson@gmail.com>
2019-01-09 12:27:19 +0100

    Added Particle class temp

--- Arduino/ES_Water/ES_Water.ino
+++ Arduino/ES_Water/ES_Water.ino
@@ -6,6 +6,9 @@
  */
 
 #include<FastLED.h>
+#include "Particle.h"
+
+#define NUM_PARTICLES 200
 
 CRGB leds[3][300];
 
@@ -15,6 +18,8 @@ int a1,a2,a3,a1l,a2l,a3l;
 int b1,b2,b3,b1l,b2l,b3l;
 int c1,c2,c3,c4,c1l,c2l,c3l,c4l;
 
+Particle particles[NUM_PARTICLES];
+
 void setup(){
   FastLED.setBrightness(192);
   FastLED.addLeds<NEOPIXEL,0>(leds[0],222);
@@ -22,6 +27,11 @@ void setup(){
   FastLED.addLeds<NEOPIXEL,4>(leds[2],300);
   FastLED.clear();
   FastLED.show();
+  for (int i = 0; i < NUM_PARTICLES; i++){
+    Particle p;
+    p.init(0,0);
+    particles[i] = p;
+  }
 }
 
 void loop(){
--- Arduino/ES_Water/Particle.h
+++ Arduino/ES_Water/Particle.h
@@ -1,3 +1,4 @@
+
 #define DAMPING 0.899
 
 class Particle {
@@ -8,39 +9,38 @@ class Particle {
     int oldY;
     int originX;
     int originY;
- 
-public:
-  Particle(int x, int y);
-  void integrate();
-  void attract(int x, int y);
-  void draw();
+  public:
+    init(int x, int y);
+    integrate();
+    attract(int x, int y);
+    draw();
 };
 
-Particle::Particle(int x, int y){
+
+Particle::init(int x, int y){
+//Particle::Particle(){
   this->x = this->oldX = this->originX = x;
   this->x = this->oldX = this->originY = y;
+  return;
 }
 
-void Particle::integrate(){
+Particle::integrate(){
   int velocityX = (this->x - this->oldX) * DAMPING;
   int velocityY = (this->y - this->oldY) * DAMPING;
   this->oldX = this->x;
   this->oldY = this->y;
   this->x += velocityX;
   this->y += velocityY;
-  return;
 }
 
-void Particle::attract(int x, int y){
+Particle::attract(int x, int y){
   int dx = x - this->x;
   int dy = y = this->y;
   int distance = sqrt(dx * dx + dy * dy);
   this->x += dx / distance;
   this->y += dy / distance;
-  return;
 }
 
-void Particle::draw(){
+Particle::draw(){
   // turn pixels on/off
-  return;
 }
