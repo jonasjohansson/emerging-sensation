bf1efa36fd4db89268b9e007ce24c42e5130ae96
bjorn <bjorn@albihn.net>
2019-01-10 02:39:55 +0100

    Adding Fade distance Blend

--- FasettUnity/Fasett/Assets/5_Scripts/Effects/Blending/OnlyShowWhenLookingAt.cs
+++ FasettUnity/Fasett/Assets/5_Scripts/Effects/Blending/OnlyShowWhenLookingAt.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class OnlyShowWhenLookingAt : MonoBehaviour {
     [SerializeField] private GameObject _target;
-    [SerializeField, Range(-1,1)] private float _dot;
+    [Header("CameraInFrontOf")]
+    [SerializeField, Range(-1,1)] private float _startBlend;
+    [SerializeField, Range(-1, 1)] private float _endBlend;
+
+    [SerializeField] public float InFrontOf;
 
     void Update () {
         Vector3 cameraPosition = Camera.main.transform.position;
         Vector3 viewDir =( transform.position - cameraPosition).normalized;
         float dot = Vector3.Dot(transform.forward, viewDir);
-        _target.SetActive(_dot > dot );
+        InFrontOf = Mathf.InverseLerp(_endBlend, _startBlend, dot);
+
+        _target.SetActive(InFrontOf > 0);
     }
 }
