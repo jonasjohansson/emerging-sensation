577c5e7ab5c32a0c62bfa1907dbe106a8b5ad959
Jonatan <jonatan@really-interactive.se>
2019-01-10 19:09:57 +0100

    Tweaking effects, adding an FPS Counter

---
+++ FasettUnity/Fasett/Assets/5_Scripts/Effects/SkyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fasett;
+
+public class SkyController : MonoBehaviour {
+    [SerializeField] private Transform[] _centerObjects;
+    [SerializeField] private Effect[] _effects;
+
+    // Use this for initialization
+    protected virtual void OnEnable () {
+        StartCoroutine(CenterEverySeconds(3));
+    }
+
+    protected virtual void OnDisable() {
+    }
+
+    private IEnumerator CenterEverySeconds(int seconds) { 
+        while (true) {
+            Vector3 center = GetCenterPoint();
+            foreach (Transform trans in _centerObjects) {
+                trans.position = center;
+            }
+            yield return new WaitForSeconds(seconds);
+        }
+    }
+
+
+    private Vector3 GetCenterPoint() {
+        Vector3 sum = Vector3.zero;
+        if(_effects == null || _effects.Length == 0) {
+            return sum;
+        }
+
+        foreach(var trigger in _effects) {
+            sum += trigger.transform.position;
+        }
+        return sum / _effects.Length;
+    }
+
+    [ContextMenu("Find in Children")]
+    private void FindInChildren() {
+        _effects = GetComponentsInChildren<Effect>(false);
+    }
+}
