808465d1bf05b3e2c41a99f9990601d76c7682a6
Björn Albihn <bjorn@albihn.net>
2019-01-11 16:22:32 +0100

    Water now more neatly attracts particles.

--- FasettUnity/Fasett/Assets/5_Scripts/Effects/Water/WaterEffect.cs
+++ FasettUnity/Fasett/Assets/5_Scripts/Effects/Water/WaterEffect.cs
@@ -10,7 +10,11 @@ public class WaterEffect : Effect {
     [SerializeField] private float _riseSpeed;
     [SerializeField] private float _fallSpeed;
 
+    [SerializeField] private AnimationCurve _riseCurve;
+    [SerializeField] private AnimationCurve _fallCurve;
+
     private bool _pressed;
+    private float _currentPress;
 
     public Action<WaterEffect> OnPressedChanged;
 
@@ -35,8 +39,15 @@ public class WaterEffect : Effect {
     protected override void Update() {
         base.Update();
     
-        if (PressAmount > 0 || _pressed) {
-            PressAmount = Mathf.Clamp01(PressAmount + Time.deltaTime / (_pressed ? _riseSpeed : -_fallSpeed));
+        if (_currentPress > 0 || _pressed) {
+            if (_pressed) { 
+                _currentPress = Mathf.Clamp01(_currentPress + Time.deltaTime /_riseSpeed);
+                PressAmount = _riseCurve.Evaluate(_currentPress); 
+            }
+            else {
+                _currentPress = Mathf.Clamp01(_currentPress + Time.deltaTime / -_fallSpeed);
+                PressAmount = _fallCurve.Evaluate(_currentPress);
+            }
 
             if (OnPressedChanged != null) {
                 OnPressedChanged(this);
