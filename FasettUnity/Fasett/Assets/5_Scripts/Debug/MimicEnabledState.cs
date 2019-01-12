f71fc6dd11b6ae79b9f4b03a3c5323500ef774ba
Björn Albihn <bjorn@albihn.net>
2019-01-12 16:08:27 +0100

    Debug dots now track if world anchor is located

--- FasettUnity/Fasett/Assets/5_Scripts/Debug/MimicEnabledState.cs
+++ FasettUnity/Fasett/Assets/5_Scripts/Debug/MimicEnabledState.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.WSA;
 
 namespace Fasett {
     [ExecuteInEditMode]
@@ -9,8 +10,18 @@ namespace Fasett {
         [SerializeField] private Renderer _renderer;
         [SerializeField] private GameObject _target;
 
+        private WorldAnchor _worldAnchor;
+
+        private void OnEnable() {
+            _worldAnchor = _target.GetComponent<WorldAnchor>();
+        }
+
         private void Update() {
             _renderer.enabled = _target.activeInHierarchy;
+            if (_worldAnchor) {
+                Color color = _worldAnchor.isLocated ? Color.green : Color.gray;
+                _renderer.material.SetColor("_Color", color);
+            }
         }
 
         [ContextMenu("Setup")]
