b8995c77f7849bdd28da5e9e43bc7481f17ea556
Jonas Johansson <jns.johansson@gmail.com>
2019-01-15 16:58:28 +0100

    Update udp-send.js

--- FasettNode/udp-send.js
+++ FasettNode/udp-send.js
@@ -83,10 +83,10 @@ function sendMessage(message) {
 }
 
 function sprayMessage(message) {
-	for (let i = 0; i < 4; i++) {
+	for (let i = 0; i < 3; i++) {
 		setTimeout(() => {
 			sendMessage(message);
-		}, i * 20);
+		}, i * 10);
 	}
 }
 
