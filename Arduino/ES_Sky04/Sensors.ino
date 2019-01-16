458d4ad83b73014d192760a6b5dae360b7224aa9
Jonas Johansson <jns.johansson@gmail.com>
2019-01-16 12:10:57 +0100

    Added remapping for stronger AR effect (look here Gustaf)

--- Arduino/ES_Sky04/Sensors.ino
+++ Arduino/ES_Sky04/Sensors.ino
@@ -1,6 +1,5 @@
 #define SMOOTHSTEP(x) ((x) * (x) * (3 - 2 * (x)))
-#define N 8.0
-#define THRESH 4
+#define N 12.0
 
 void readAdvanced(byte index, float &val, float &oldVal, float min, float max){
   float newVal = analogRead(pins[index]);
@@ -14,14 +13,19 @@ void readAdvanced(byte index, float &val, float &oldVal, float min, float max){
   }
 
   int diff = abs(floor(val) - floor(oldVal));
-  if (diff > 0){
-    s(index,val);
+  if (diff > 1){
+    // re-mapping, look here Gustaf
+    int mapped = map(val,24,222,0,255);
+    mapped = constrain(mapped,0,255);
+    s(index,mapped);
+    //s(index,val);
   }
 
   oldVal = val;
 }
 
 void s(byte pin, int val){
+  if (isCalibrating) return;
   Serial.print("S"+String(pin));
   Serial.print(' ');
   Serial.println(val);
